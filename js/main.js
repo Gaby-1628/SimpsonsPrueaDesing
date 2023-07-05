a79d716a3a5eced41696173d26ad956413400303
Gabriela Marisol Rodriguez Dos Santos <gabriela.rodriguez@outit.com.ar>
2023-07-05 15:29:32 -0300

    Primer commit

---
+++ js/main.js
@@ -0,0 +1,32 @@
+document.addEventListener('DOMContentLoaded', async() => {
+    charactersList.innerHTML = '<h1>Loading</h1>'
+    const data = await showUrl()
+    show = data
+    showCharacters(show)
+})
+
+let show = []
+
+const header = document.querySelector('#header-box')
+const charactersList = document.querySelector('#container')
+const input = document.querySelector('#search_input')
+
+async function showUrl() {
+    const response = await fetch('https://thesimpsonsquoteapi.glitch.me/quotes?count=1000&character=$')
+    return await response.json()
+}
+
+input.addEventListener('keyup', e => {
+    const newData = show.filter(character => character.character.includes(input.value))
+    showCharacters(newData)
+})
+
+const createCharacters = container => container.map(character => `<div><h2>${character.character}</h2><p>${character.quote}</p><img src="${character.image}"></div>`).join('')
+
+function showCharacters(container) {
+    const itemCharacters = createCharacters(container)
+    charactersList.innerHTML = itemCharacters
+}
+
+
+
